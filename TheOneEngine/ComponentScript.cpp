489ef17f81632159c9388bb96853acbc6e9b7819
Sergi PR <t3m1X@users.noreply.github.com>
2024-01-31 14:02:06 +0100

    Updated vcxproj

--- TheOneEngine/ComponentScript.cpp
+++ TheOneEngine/ComponentScript.cpp
@@ -4,7 +4,7 @@
 ComponentScript::ComponentScript(std::shared_ptr<GameObject> containerGO, const char* path) : 
 	Component(containerGO, ComponentType::Script) {
 	unique = false;
-	path_ = Fileutils::GetWorkingDir() + path;
+	path_ = Fileutils::GetWorkingDir() + path; // We need an absolute path
 	script_name_ = Fileutils::GetNameFromPath(path);
 }
 
