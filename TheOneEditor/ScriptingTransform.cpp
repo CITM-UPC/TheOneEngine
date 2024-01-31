3b2903a576466ada2e650b32aabbfe6ccc26e88d
Sergi PR <t3m1X@users.noreply.github.com>
2024-01-31 11:15:13 +0100

    Created transform functions for scripting

---
+++ TheOneEditor/ScriptingTransform.cpp
@@ -0,0 +1,74 @@
+#include "ScriptingTransform.h"
+#include "SceneManager.h"
+#include "App.h"
+#include "luadefs.h"
+#include "../TheOneEngine/Transform.h"
+
+luabridge::LuaRef ScriptingTransform::GetPosition(unsigned int go_UUID, lua_State* lua_state) {
+	vec3 position = vec3(0.0f);
+	std::shared_ptr<GameObject> go = app->sceneManager->FindGOByUID(go_UUID);
+	if (go) {
+		Transform* m_transform = go->GetComponent<Transform>();
+		position = m_transform->getPosition();
+	} else {} // TODO: Throw an error maybe?
+
+	luabridge::LuaRef table = luabridge::newTable(lua_state);
+	table.append(position.x);
+	table.append(position.y);
+	table.append(position.z);
+
+	return table;
+}
+
+void ScriptingTransform::SetPosition(unsigned int go_UUID, float x, float y, float z) {
+	std::shared_ptr<GameObject> go = app->sceneManager->FindGOByUID(go_UUID);
+	if (go) {
+		Transform* m_transform = go->GetComponent<Transform>();
+		m_transform->setPosition(vec3(x, y, z));
+	}
+	else {} // TODO: Throw an error maybe?
+}
+
+void ScriptingTransform::Translate(unsigned int go_UUID, float x, float y, float z) {
+	std::shared_ptr<GameObject> go = app->sceneManager->FindGOByUID(go_UUID);
+	if (go) {
+		Transform* m_transform = go->GetComponent<Transform>();
+		m_transform->translate(vec3(x, y, z));
+	}
+	else {} // TODO: Throw an error maybe?
+}
+
+luabridge::LuaRef ScriptingTransform::GetRotation(unsigned int go_UUID, lua_State* lua_state) {
+	vec3 rotation = vec3(0.0f);
+	std::shared_ptr<GameObject> go = app->sceneManager->FindGOByUID(go_UUID);
+	if (go) {
+		Transform* m_transform = go->GetComponent<Transform>();
+		rotation = m_transform->getEulerAngles();
+	}
+	else {} // TODO: Throw an error maybe?
+
+	luabridge::LuaRef table = luabridge::newTable(lua_state);
+	table.append(rotation.x);
+	table.append(rotation.y);
+	table.append(rotation.z);
+
+	return table;
+}
+
+void ScriptingTransform::SetRotation(unsigned int go_UUID, float x, float y, float z) {
+	std::shared_ptr<GameObject> go = app->sceneManager->FindGOByUID(go_UUID);
+	if (go) {
+		Transform* m_transform = go->GetComponent<Transform>();
+		m_transform->setRotation(vec3(x, y, z));
+	}
+	else {} // TODO: Throw an error maybe?
+}
+
+void ScriptingTransform::Rotate(unsigned int go_UUID, float x, float y, float z) {
+	std::shared_ptr<GameObject> go = app->sceneManager->FindGOByUID(go_UUID);
+	if (go) {
+		Transform* m_transform = go->GetComponent<Transform>();
+		m_transform->rotate(vec3(x, y, z));
+	}
+	else {} // TODO: Throw an error maybe?
+}
---
+++ TheOneEditor/ScriptingTransform.h
@@ -0,0 +1,21 @@
+#ifndef __SCRIPTINGTRANSFORM_H__
+#define __SCRIPTINGTRANSFORM_H__
+
+class lua_State;
+namespace luabridge
+{
+	class LuaRef;
+}
+
+namespace ScriptingTransform
+{
+luabridge::LuaRef	GetPosition			(unsigned int go_UUID, lua_State* lua_state);
+void				SetPosition			(unsigned int go_UUID, float x, float y, float z);
+void				Translate			(unsigned int go_UUID, float x, float y, float z);
+
+luabridge::LuaRef	GetRotation	(unsigned int go_UUID, lua_State* lua_state);
+void				SetRotation	(unsigned int go_UUID, float x, float y, float z);
+void				Rotate		(unsigned int go_UUID, float x, float y, float z);
+}
+
+#endif
