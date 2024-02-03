c0acb446450d32f3169075f5379667a42636e414
Sergi PR <t3m1X@users.noreply.github.com>
2024-02-03 20:39:56 +0100

    Added functions to get global position and rotation

--- TheOneEditor/ScriptingTransform.cpp
+++ TheOneEditor/ScriptingTransform.cpp
@@ -22,6 +22,25 @@ luabridge::LuaRef ScriptingTransform::GetPosition(unsigned int go_UUID, lua_Stat
 	return table;
 }
 
+luabridge::LuaRef ScriptingTransform::GetGlobalPosition(unsigned int go_UUID, lua_State* lua_state) const {
+	vec3 position = vec3(0.0f);
+	std::shared_ptr<GameObject> go = app->sceneManager->FindGOByUID(go_UUID);
+	if (go) {
+		Transform* m_transform = go->GetComponent<Transform>();
+		position = m_transform->getGlobalPosition();
+	}
+	else {
+		LOG(LogType::LOG_WARNING, "GameObject with UID %d does not exist.", go_UUID);
+	}
+
+	luabridge::LuaRef table = luabridge::newTable(lua_state);
+	table.append(position.x);
+	table.append(position.y);
+	table.append(position.z);
+
+	return table;
+}
+
 void ScriptingTransform::SetPosition(unsigned int go_UUID, float x, float y, float z) {
 	std::shared_ptr<GameObject> go = app->sceneManager->FindGOByUID(go_UUID);
 	if (go) {
@@ -45,6 +64,25 @@ void ScriptingTransform::Translate(unsigned int go_UUID, float x, float y, float
 }
 
 luabridge::LuaRef ScriptingTransform::GetRotation(unsigned int go_UUID, lua_State* lua_state) const{
+	vec3 rotation = vec3(0.0f);
+	std::shared_ptr<GameObject> go = app->sceneManager->FindGOByUID(go_UUID);
+	if (go) {
+		Transform* m_transform = go->GetComponent<Transform>();
+		rotation = m_transform->getLocalEulerAngles();
+	}
+	else {
+		LOG(LogType::LOG_WARNING, "GameObject with UID %d does not exist.", go_UUID);
+	}
+
+	luabridge::LuaRef table = luabridge::newTable(lua_state);
+	table.append(rotation.x);
+	table.append(rotation.y);
+	table.append(rotation.z);
+
+	return table;
+}
+
+luabridge::LuaRef ScriptingTransform::GetGlobalRotation(unsigned int go_UUID, lua_State* lua_state) const {
 	vec3 rotation = vec3(0.0f);
 	std::shared_ptr<GameObject> go = app->sceneManager->FindGOByUID(go_UUID);
 	if (go) {
