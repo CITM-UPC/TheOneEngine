1899cc0d1fee1eb33752ad2121e24d80d096c596
Sergi PR <t3m1X@users.noreply.github.com>
2024-02-07 01:42:42 +0100

    Fixed silly mistakes, added logging errors

--- TheOneEditor/Shader.cpp
+++ TheOneEditor/Shader.cpp
@@ -1,5 +1,5 @@
 #include "Shader.h"
-#include "glew.h"
+#include "GL/glew.h"
 #include "Log.h"
 
 namespace ShaderUtils
@@ -17,7 +17,7 @@ namespace ShaderUtils
 	int CreateFragmentShader(unsigned int& fragment_id, const char* fragment_shader) {
 		int success = GL_FALSE;
 
-		fragment_id = glCreateShader(GL_VERTEX_SHADER);
+		fragment_id = glCreateShader(GL_FRAGMENT_SHADER);
 		glShaderSource(fragment_id, 1, &fragment_shader, NULL);
 		glCompileShader(fragment_id);
 		glGetShaderiv(fragment_id, GL_COMPILE_STATUS, &success);
@@ -38,17 +38,21 @@ void Shader::CompileShader()
 {
     int success = GL_FALSE;
 	bool failed = false;
+	const int k_log_length = 512;
+	char error_log[k_log_length];
 
 	success = ShaderUtils::CreateVertexShader(vertex_id, vertex_shader.c_str());
 	if (success == GL_FALSE) {
-		LOG(LogType::LOG_ERROR, "Failed to compile vertex shader");
+		glGetShaderInfoLog(vertex_id, k_log_length, NULL, error_log);
+		LOG(LogType::LOG_ERROR, "Failed to compile vertex shader, error %s", error_log);
 		glDeleteShader(vertex_id);
 		return;
 	}
 
 	success = ShaderUtils::CreateFragmentShader(fragment_id, fragment_shader.c_str());
 	if (success == GL_FALSE) {
-		LOG(LogType::LOG_ERROR, "Failed to compile fragment shader");
+		glGetShaderInfoLog(fragment_id, k_log_length, NULL, error_log);
+		LOG(LogType::LOG_ERROR, "Failed to compile fragment shader, error %s", error_log);
 		glDeleteShader(fragment_id);
 		glDeleteShader(vertex_id);
 		return;
@@ -61,7 +65,8 @@ void Shader::CompileShader()
 	glGetProgramiv(id, GL_LINK_STATUS, &success);
 
 	if (success == GL_FALSE) {
-		LOG(LogType::LOG_ERROR, "Failed to create shader program");
+		glGetProgramInfoLog(id, k_log_length, NULL, error_log);
+		LOG(LogType::LOG_ERROR, "Failed to create shader program: %s", error_log);
 	}
 
 	// These are now compiled so we can delete them
